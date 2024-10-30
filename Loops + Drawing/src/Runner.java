e0cf0619c3778ed9da1ec1fa6a7287cba102a245
1903715 <1903715@01-18100.pusd.dom>
2024-10-30 10:29:21 -0700

    ll

--- Loops + Drawing/src/Runner.java
+++ Loops + Drawing/src/Runner.java
@@ -1,4 +1,5 @@
 import java.awt.BasicStroke;
+
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -14,8 +15,20 @@ public class Runner extends JPanel{
 	public void paint(Graphics g) {
 		
 		Graphics2D g2 = (Graphics2D) g;
-        g2.setStroke(new BasicStroke(5));
+        g2.setStroke(new BasicStroke(1));
+
         
+        	for(int count=0; count<27; count++) {
+        		g.drawLine(1000, 650-count*25, 1000-count*25, 0);
+        	}
+        	for(int count2=0; count2<27; count2++) {
+        		g.drawLine(650-count2*25, 0, 0, count2*25);
+        	}
+        	int count3=0;
+        	while(count3<27) {
+        		count3++;
+        		g.drawLine(0, count3*25, count3*25, 650);
+        	}
 		 
 		//1) draw a concentric rectangle that's 10pixels from all sides of the "gray"
 		//portion of the GUI
@@ -23,16 +36,79 @@ public class Runner extends JPanel{
 		
 		
 		//2) Draw a horizontal line to bisect the Rectangle
-		g2.drawLine(10, 270, 770, 270);
+		g2.drawLine(10, 270, 775, 270);
 		
 		
 		//3) Draw a vertical line to bisect the shape again
-		g2.drawLine(765/2, 10, 765/2,550 );
+		g2.drawLine(765/2, 10, 765/2, 550 );
 		
 		
 		//4) small rectangles on top-left
-		g2.drawLine(765/4, 10, 765/4, 550/2-10);
+		//g2.drawLine(765/4, 10, 765/4, 550/2-10);
 		
+		//top left (100 random lines)
+		int count1 = 0;
+		while(count1 <= 100) {
+			count1++;
+			g2.setColor( new Color((int) (Math.random()*(255 - 0 + 1)) + 0,
+					(int) (Math.random()*(255 - 0 + 1)) + 0, 
+					(int) (Math.random()*(255 - 0 + 1)) + 0));
+			
+			g2.drawLine((int) (Math.random()*(765/2 - 10 + 1) ) + 10, 
+					(int) (Math.random()*(270 - 10 + 1) ) + 10, 
+					(int) (Math.random()*(765/2 - 10 + 1) ) + 10, 
+					(int) (Math.random()*(270 - 10 + 1) ) + 10);
+			
+		}
+		
+		//top right (100 random squares)
+		int count2 = 0;
+		while(count2 <= 100) {
+			count2++;
+			g2.setColor( new Color((int) (Math.random()*(255 - 0 + 1)) + 0,
+					(int) (Math.random()*(255 - 0 + 1)) + 0, 
+					(int) (Math.random()*(255 - 0 + 1)) + 0));
+			
+			g2.fillRect((int) (Math.random()*(715 - 765/2 + 1) ) + 765/2, 
+					(int) (Math.random()*(220 - 10 + 1) ) + 10,
+					50, 
+					50);
+		}
+		
+		//bottom left (100 random circles)
+		int count3 = 0;
+		while(count3 <= 100) {
+			count3++;
+			g2.setColor( new Color((int) (Math.random()*(255 - 0 + 1)) + 0,
+					(int) (Math.random()*(255 - 0 + 1)) + 0, 
+					(int) (Math.random()*(255 - 0 + 1)) + 0));
+			
+			int diameter = (int) (Math.random()*(200 - 0 + 1)) + 0;
+			
+			g2.drawOval((int) (Math.random()*((765/2 - diameter - 1) - 10 + 1) ) + 10, 
+					(int) (Math.random()*((540 - diameter - 1) - 270 + 1) ) + 270, 
+					diameter, 
+					diameter);
+		}
+		
+		//bottom right (box thing)
+		g2.setColor(Color.yellow);
+		g2.fillRect(500, 350, 100, 100);
+		
+		g2.setColor(Color.red);
+		g2.fillRect(550, 400, 100, 100);
+		
+		g2.setColor(Color.blue);
+		int [] bluex = {600, 600, 650};
+		int [] bluey = {350, 400, 400};
+		int bluePoints = 3;
+		g2.fillPolygon(bluex, bluey, bluePoints);
+		
+		g2.setColor(Color.green);
+		int [] greenx = {500, 500, 550, 550};
+		int [] greeny = {350, 450, 500, 400};
+		int greenpoints = 4;
+		g2.fillPolygon(greenx, greeny, greenpoints);
 		
 		/* 
 		 * int var = 0; 						<---- control variable
